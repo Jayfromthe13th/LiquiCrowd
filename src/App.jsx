b2848f756aa34a3b294733a16d26a708d21ba0d6
norrischebl <norrischebl@gmail.com>
2024-01-30 17:27:14 -0800

    Day 3

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import "./App.css";
+import TopNav from "./components/layout/TopNav";
+import Home from "./pages/home";
+import { CssBaseline } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <TopNav />
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { ThemeProvider } from "@emotion/react";
+import { theme } from "./styles/theme.jsx";
+
+// import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
